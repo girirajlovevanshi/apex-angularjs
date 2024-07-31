6af1aec3abf4f569348392f4358f156ff1fd7d12
giri <girirajlovewanshi@gmail.com>
2024-08-01 01:44:09 +0530

    task done

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { todolistcomponent } from "./todo-list/todolist.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, todolistcomponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ todo-app/src/app/todo-list/todolist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'todo-list',
+    standalone: true,
+    templateUrl: './todolist.component.html',
+    styleUrl: './todolist.component.css'
+})
+export class todolistcomponent {
+    
+}
